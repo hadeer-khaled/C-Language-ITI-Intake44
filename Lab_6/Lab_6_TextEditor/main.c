819da0881fcf40e5793df1f7651d66c89bf2ce35
Hadeer Khaled <61336360+hadeer-khaled@users.noreply.github.com>
2023-10-25 23:49:07 +0300

    Update main.c

--- Lab_6/Lab_6_TextEditor/main.c
+++ Lab_6/Lab_6_TextEditor/main.c
@@ -8,14 +8,17 @@
 #define SIZE 100
 #define RIGHT 77
 #define LEFT 75
+#define HOME 71
+#define END 79
+#define ESC 27
+#define DELETE 8
+#define ENTER 13
 
 void gotoxy(int x,int y);
-
 int main()
 {
-
-    char arr[SIZE];
-    char key = ' ';
+    char arr[SIZE] = {0};
+    char key;
     int arr_indx = 0 ;
     int x_coord = 0;
     int y_coord = 1 ;
@@ -25,44 +28,59 @@ int main()
     while(1)
     {
         key = getch();
-        if( -32 == key ) // is it a special char ??
-        {
-            key = getch();
+        switch(key){
+            case -32: // is it a special char ??
+                key = getch();
+                switch(key) { // yes, it is a special char,but which one ?
+                    case LEFT:
+                        if(x_coord> 0){
+                            x_coord--;
+                            gotoxy(x_coord,y_coord);
+                        }
+                        else{}
+                        break;
+                    case RIGHT:
+                        x_coord++;
+                        gotoxy(x_coord,y_coord);
+                        break;
+                    case HOME:
+                        gotoxy(0,y_coord);
+                        break;
+                    case END:
+                        gotoxy(arr_indx,y_coord);
+                        break;
 
-            switch(key) // yes, it is a special char,but which one ?
-            {
-            case LEFT:
-                if(x_coord> 0){
-                x_coord--;
-                gotoxy(x_coord,y_coord);
-                }
-                else{}
+                    }//end switch
+                //end case -32
                 break;
-
-            case RIGHT:
+                    case DELETE:
+                            for (int i = x_coord; i < arr_indx; i++) {
+                                arr[i] = arr[i + 1];
+                                }
+                                arr_indx--;
+                                x_coord =arr_indx;
+                                system("cls");
+                                printf("Enter a sentence:\n");
+                                printf("%s" , arr);
+                                gotoxy(x_coord ,y_coord );
+                        break;
+            case ENTER:  // is it enter ??
+                printf("\n%s", arr) ;
+                y_coord++;
+                //gotoxy(arr_indx ,y_coord );
+                break;
+            default:
+                printf("%c", key);
+                arr[x_coord] = key ;
+                arr_indx++;
                 x_coord++;
-                gotoxy(x_coord,y_coord);
+                //gotoxy(5 ,6 );
+                //printf("%d" , arr_indx);
+                //gotoxy(x_coord ,y_coord );
                 break;
-            }//end switch
-
-        }//end if
-        else if( 13 == key) // is it enter ??
-        {
-            printf("\n%s", arr) ;
-            break;
-        }
-
-        else
-        {
-            printf("%c", key);
-            arr[x_coord] = key ;
-            x_coord++;
-        } //end else
-
+            } // end switchq
     }//end while
-
 } //end main
-
 
 void gotoxy(int x,int y)
 {
